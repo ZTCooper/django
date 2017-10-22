98fd3549ce57a2e1a6e60105d3b0ce5d920fc207
ZTCooper <ztcooper@outlook.com>
2017-10-22 22:29:06 +0800

    定制展示形式

--- django-by-example/mysite/blog/admin.py
+++ django-by-example/mysite/blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):		#展示形式
+	list_display = ('title', 'slug', 'author', 'publish', 'status')		
+	list_filter = ('status', 'created', 'publish', 'author')	#侧边栏
+	search_fields = ('title', 'body')	#搜索框
+	prepopulited_fields = {'slug': ('title', )}
+	raw_id_fields = ('author', )
+	date_hierarchy = 'publish'
+	odering = ['status', 'publish']
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
