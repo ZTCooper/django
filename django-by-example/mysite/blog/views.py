b20a47e41e9c8886536b797cedd38fea118fd0d3
ZTCooper <ztcooper@outlook.com>
2017-11-15 20:26:43 +0800

    添加页码

--- django-by-example/mysite/blog/views.py
+++ django-by-example/mysite/blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
 
 def post_list(request):
 	posts = Post.published.all()
-	return render(request, 'blog/post/list.html', {'posts': posts})
+	object_list = Post.published.all()
+	paginator = Paginator(object_list, 3)	#每页3个post
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:		#不是整数
+		posts = paginator.page(1)	#返回第一页
+	except EmptyPage:	#超出最大
+		posts = paginator.page(paginator.num_pages)		#返回最后一页
+	return render(request, 'blog/post/list.html', {'page':page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
 	post = get_bject_or_404(Post,
